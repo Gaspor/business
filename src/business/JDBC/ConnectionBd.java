2f816944c26779578ee1dd311261fdd3bc477d31
Gaspor <gaspor991@gmail.com>
2021-04-19 15:21:14 -0300

    add: env

--- src/business/JDBC/ConnectionBd.java
+++ src/business/JDBC/ConnectionBd.java
@@ -6,32 +6,16 @@ import java.sql.SQLException;
 
 //Conexão ao Banco de dados
 public class ConnectionBd {
-
     public Connection getConnection() {
-        String nomeUsuario = "postgres"; //Usuário do servidor padrão
-        String senhaUsuario = "hZjbg}e]Xniyup"; // Senha do servidor
-        String enderecoServidor = "localhost:5433";
-        String nomeBanco = "business";
-
         try {
-            System.out.println("Tentando conexão com o banco Gaspor");
-            return DriverManager.getConnection("jdbc:postgresql://" + enderecoServidor
-                    + "/" + nomeBanco, nomeUsuario, Decode.decode(senhaUsuario));
+            env env = new env();
+            System.out.println("Tentando conexão com o banco de dados");
+            return DriverManager.getConnection("jdbc:postgresql://" + env.enderecoServidor + ":" + env.port
+                    + "/" + env.nomeBanco, env.nomeUsuario, env.senhaUsuario);
 
-        } catch (SQLException ex) {
-            System.out.println("Sem conexão com o banco Gaspor");
-            
-            
-            try {
-                senhaUsuario = "4*+/;B61,.9B";
-                System.out.println("Tentando conexão com o banco Friday");
-                return DriverManager.getConnection("jdbc:postgresql://" + enderecoServidor
-                        + "/" + nomeBanco, nomeUsuario, Decode.decode(senhaUsuario));
-
-            } catch (SQLException e) {
-                System.out.println("Erro, sem conexão com o banco de dados");
-                throw new RuntimeException(e);
-            }
+        } catch (SQLException e) {
+            System.out.println("Erro, sem conexão com o banco de dados");
+            throw new RuntimeException(e);
         }
     }
 
